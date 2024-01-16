378d9b3c11b454df5afad3db2f6eb05ae84690e8
Jesse Harlan <jesse.harlan@centralia.edu>
2024-01-16 10:55:44 -0800

    Add project files.

---
+++ TodoApp/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int TodoId { get; set; }
+        public string Description { get; set; }
+        public bool Complete { get; set; }
+    }
+}
